2d3442cc976c26bd929357e4421edcda509ebb56
Kauê Wendt Sabino <kaue.sabino@modalgr.com.br>
2024-05-13 14:11:29 -0300

    feat: add Id to User

--- src/repositories/get-users/mongo-get.users.ts
+++ src/repositories/get-users/mongo-get.users.ts
@@ -1,15 +1,12 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Kaue",
-        lastName: "Sabino",
-        email: "kaue.sabino@hotmail.com",
-        password: "123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<User>("users")
+      .find({})
+      .toArray();
   }
 }
